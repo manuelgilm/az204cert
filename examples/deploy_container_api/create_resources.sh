29a7b4a2dd3bac0be3c9b675afee71c10767bf3f
Manuel Gil <manuelgilsitio@gmail.com>
2025-06-30 21:30:53 -0500

    adding simple fastapi application and dockerfile to build the image

---
+++ examples/deploy_container_api/create_resources.sh
@@ -0,0 +1,6 @@
+# create resource groug 
+az group create --name SimpleApiRG --location brazilsouth # this resource group already exists 
+# create service plan for the simple api (already exists)
+az appservice plan create --name SimpleApiPlan --resource-group SimpleApiRG --sku S1 --is-linux
+# create web app for the simple API (containerized)
+az webapp create --name simpleapicontainer --resource-group SimpleApiContainer --plan SimpleApiPlan --basic-auth Enabled

5fc2d8d0dd634f464021c49ccf389cdac779f881
Manuel Gil <manuelgilsitio@gmail.com>
2025-07-02 07:53:33 -0500

    updating create_resource.sh script with comments

--- examples/deploy_container_api/create_resources.sh
+++ examples/deploy_container_api/create_resources.sh
@@ -3,4 +3,5 @@ az group create --name SimpleApiRG --location brazilsouth
 # create service plan for the simple api (already exists)
 az appservice plan create --name SimpleApiPlan --resource-group SimpleApiRG --sku S1 --is-linux
 # create web app for the simple API (containerized)
-az webapp create --name simpleapicontainer --resource-group SimpleApiContainer --plan SimpleApiPlan --basic-auth Enabled
+# Note: It is better to use this within a CI/CD pipeline where the image is built and pushed to a container registry. Secrets for the registry can be managed securely.
+az webapp create --name simpleapicontainer --resource-group SimpleApiRG --plan SimpleApiPlan -i <repository/image-name> -s <username> -w <password> 

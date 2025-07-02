1e45bdb476fc9ae438ceca348cc096d42802fe54
Manuel Gil <manuelgilsitio@gmail.com>
2025-07-02 07:53:18 -0500

    updating create_resource.sh script with comments

--- examples/deploy_container_api/create_resources.sh
+++ examples/deploy_container_api/create_resources.sh
@@ -1,5 +1,5 @@
 # create resource groug 
-az group create --name SimpleApiRG --location brazilsouth # this resource group already exists 
+az group create --name SimpleApiRG --location brazilsouth
 # create service plan for the simple api (already exists)
 az appservice plan create --name SimpleApiPlan --resource-group SimpleApiRG --sku S1 --is-linux
 # create web app for the simple API (containerized)

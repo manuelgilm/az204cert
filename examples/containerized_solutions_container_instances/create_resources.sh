bc44b402a738d0003f1663096c60223d98e9f50a
Manuel Gil <manuelgilsitio@gmail.com>
2025-07-05 20:48:25 -0500

    adding restart policy command

--- examples/containerized_solutions_container_instances/create_resources.sh
+++ examples/containerized_solutions_container_instances/create_resources.sh
@@ -18,3 +18,14 @@ az container show --resource-group containerized-solutions \
     --name simpleapigs \
     --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" \
     --out table     
+
+# restart policy
+az container create --resource-group <resource group name> \
+    --name <container name> \
+    --image <image name> \
+    --restart-policy OnFailure
+
+# Available policies:
+# - Always: Containers in the container group are always restarted. This is the default setting applied when no restart policy is specified at container creation.
+# - OnFailure: Containers in the container group are restarted only if they exit with a non-zero exit code.
+# - Never: Containers in the container group are never restarted, regardless of their exit code.  The containers run at most once.
